d4052c0667ee9a36ad0bcf282d05876da4a832f0
Sam Kachergius <skachergius@gmail.com>
2024-08-13 12:56:33 -0500

    added basic routes and server file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api)
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  return res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
